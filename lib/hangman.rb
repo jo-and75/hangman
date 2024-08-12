04bc7242eef7021aa840c5d8e7ddf4aee4b30be6
jo-and75 <jaotika@gmail.com>
2024-08-12 11:42:50 +0300

    Add condition to only select words between 5 and 12 characters

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,11 @@
 puts 'Game loaded' 
-
-lines = File.open('google-10000-english-no-swears.txt', 'r') 
-lines.each do |line| 
-  puts line 
+desired_words = []
+lines = File.open('google-10000-english-no-swears.txt', 'r')
+lines.each do |line|  
+  word = line.to_s.strip
+   if word.length >= 5 && word.length < 13
+    desired_words << word
+   end
 end
+
+puts desired_words
